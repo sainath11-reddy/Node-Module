0f0df1c72700315481cc5b89bf3acc0eb1562aca
sainath11-reddy <sainath11reddy@gmail.com>
2022-10-12 17:53:13 +0530

    task7 commit

--- task1.js
+++ task1.js
@@ -1,5 +1,18 @@
 console.log("Hello World");
 const http = require("http");
-const server= http.createServer((req,res)=>{console.log("Sainath")});
+const server= http.createServer((req,res)=>{
+    console. log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    if(req.url=='/home') 
+    res.write( '<body><h1>Welcome home</h1></body>');
+    if(req.url=='/about')
+    res.write( '<body><h1>Welcome to About Us page</h1></body>')
+    if(req.url=='/node')
+    res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+    res.write('</html>');
+    res.end();
+});
 server.listen(4000);
 
